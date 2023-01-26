9ba841ba37b30eb4bd9b4674c87a49d6185ebe89
holylance <holylance82@gmail.com>
2023-01-26 17:55:14 +0100

    Added error message

--- app/src/main/java/com/cockerspaniel/luncnow/screen/main/model/LuncDescriptionItem.kt
+++ app/src/main/java/com/cockerspaniel/luncnow/screen/main/model/LuncDescriptionItem.kt
@@ -2,13 +2,9 @@ package com.cockerspaniel.luncnow.screen.main.model
 
 import com.cockerspaniel.luncnow.util.listadapter.ListItemModel
 import com.cockerspaniel.luncnow.util.listadapter.ListItemTypeFactory
-import java.math.BigDecimal
 
-data class BurnLuncItem(
-    val name: String,
-    val amountNum: BigDecimal,
-    val amount: String,
-    val ranking: Int = 0
+data class LuncDescriptionItem(
+    val id: Long = 0L,
 ) : ListItemModel {
     override fun type(typeFactory: ListItemTypeFactory) = typeFactory.type(this)
 }
